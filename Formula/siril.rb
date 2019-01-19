635800af72cefe07a0f98af0d8ea66a4b744bc61
Suho Lee <susemeee@gmail.com>
2019-01-19 22:49:03 +0900

    Update siril.rb
    - 0.9.10
    - OpenMP support

--- Formula/siril.rb
+++ Formula/siril.rb
@@ -1,10 +1,9 @@
 class Siril < Formula
   desc "Astronomical image processing tool"
   homepage "https://free-astro.org/index.php/Siril"
-  url "https://free-astro.org/download/siril-0.9.9.tar.bz2"
-  sha256 "7958985393eca33b2db173090af78a46e42a7daefe7f6eaa7efa4ba261fa46f3"
-  revision 3
-  head "http://free-astro.org/svn/siril/", :using => :svn
+  url "https://free-astro.org/download/siril-0.9.10.tar.bz2"
+  sha256 "caf9800a442bbe3991e820ffc66f41b453c6866f510e2934d236788c78f5be29"
+  head "https://gitlab.com/free-astro/siril.git"
 
   bottle do
     sha256 "9a69ae2805e66e70525d009a6f301aaeaf3b000d36b5810b8fb2d2f97d0ef15c" => :mojave
@@ -26,19 +25,13 @@ class Siril < Formula
   depends_on "gtk-mac-integration"
   depends_on "jpeg"
   depends_on "libconfig"
+  depends_on "libomp"
   depends_on "libraw"
   depends_on "librsvg"
   depends_on "libsvg"
   depends_on "netpbm"
   depends_on "opencv"
   depends_on "openjpeg"
-
-  # Upstream fix for compilation with OpenCV 4
-  # Remove in next version
-  patch do
-    url "https://gitlab.com/free-astro/siril/commit/c23c2cc829b2ad9444ccefeb865f7e1b3d49c282.diff"
-    sha256 "22e179e832c7f6a28d5f2bfb3953be477b15450df41ceeb353b77376bec7e048"
-  end
 
   needs :cxx11
 
@@ -47,7 +40,7 @@ class Siril < Formula
 
     # siril uses pkg-config but it has wrong include paths for several
     # headers. Work around that by letting it find all includes.
-    ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include"
+    ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include -Xpreprocessor -fopenmp -lomp"
 
     system "./autogen.sh", "--prefix=#{prefix}"
     system "make", "install"
